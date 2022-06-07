594b3aafc7623498de60c97beda93dde92a774c5
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-07 15:08:37 -0500

    Create divideinput.cpp

---
+++ divideinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {;
+  int a;
+  int b;
+  cout << "Type a's value:";
+  cin >> a;
+  cout << "Type b's value:";
+  cin >> b;
+  cout << "Answer:" << a/b;
+}
